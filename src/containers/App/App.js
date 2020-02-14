f1cb2b33be7aeb064a37c533be89216398b038e1
Mar Balibrea <marbalibrea8@gmail.com>
2020-02-14 10:21:19 +0100

    first commit

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './App.module.scss';
+import TimeSlider from '../../components/TimeSlider/TimeSlider.js';
 
 function App() {
 
   return (
     <div className={classes.App}>
+      <TimeSlider />
     </div >
   );
 }
