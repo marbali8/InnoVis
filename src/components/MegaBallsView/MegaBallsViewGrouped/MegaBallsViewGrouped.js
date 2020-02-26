0443d59780e309ddeb04329e1cc8b8ecbf169a09
hanneysaurus <hanneysaurus@hotmail.com>
2020-02-26 12:07:19 +0100

    detail display for hovering over balls

--- src/components/MegaBallsView/MegaBallsViewGrouped/MegaBallsViewGrouped.js
+++ src/components/MegaBallsView/MegaBallsViewGrouped/MegaBallsViewGrouped.js
@@ -1,8 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import data from '../../../data/companies_yearly_data.json';
 import * as d3 from 'd3';
 
-const MegaBallsView = ({ height = 1000, width = 1000, showBorderOfBallBox = true, margin = { left: 0, right: 0, top: 0, bottom: 0 }, onYearClicked }) => {
+const MegaBallsView = ({
+                           height = 1000, width = 1000, showBorderOfBallBox = true, margin = {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+    }, onYearClicked
+                       }) => {
     const svgRef = useRef();
     var year_choice = onYearClicked;
 
@@ -14,8 +21,8 @@ const MegaBallsView = ({ height = 1000, width = 1000, showBorderOfBallBox = true
     const bvm = 1 / 10 * height;
     const bhm = 1 / 10 * width;
     const ballBox = {
-        min: { x: bhm, y: bvm },
-        max: { x: width - bhm, y: height - bvm }
+        min: {x: bhm, y: bvm},
+        max: {x: width - bhm, y: height - bvm}
     };
 
     //const sim = useRef();
@@ -90,57 +97,81 @@ const MegaBallsView = ({ height = 1000, width = 1000, showBorderOfBallBox = true
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2010)),
                         secsize: Math.sqrt(data[i].employees_2010),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2010,
+                        revenue: data[i].revenue_2010
+                    };
                     break;
                 case 2011:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2011)),
                         secsize: Math.sqrt(data[i].employees_2011),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2011,
+                        revenue: data[i].revenue_2011
+                    };
                     break;
                 case 2012:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2012)),
                         secsize: Math.sqrt(data[i].employees_2012),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2012,
+                        revenue: data[i].revenue_2012
+                    };
                     break;
                 case 2013:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2013)),
                         secsize: Math.sqrt(data[i].employees_2013),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2013,
+                        revenue: data[i].revenue_2013
+                    };
                     break;
                 case 2014:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2014)),
                         secsize: Math.sqrt(data[i].employees_2014),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2014,
+                        revenue: data[i].revenue_2014
+                    };
                     break;
                 case 2015:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2015)),
                         secsize: Math.sqrt(data[i].employees_2015),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2015,
+                        revenue: data[i].revenue_2015
+                    };
                     break;
                 case 2016:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2016)),
                         secsize: Math.sqrt(data[i].employees_2016),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2016,
+                        revenue: data[i].revenue_2016
+                    };
                     break;
                 case 2017:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2017)),
                         secsize: Math.sqrt(data[i].employees_2017),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2017,
+                        revenue: data[i].revenue_2017
+                    };
                     break;
                 case 2019:
                 case 2020:
@@ -148,22 +179,27 @@ const MegaBallsView = ({ height = 1000, width = 1000, showBorderOfBallBox = true
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2018)),
                         secsize: Math.sqrt(data[i].employees_2018),
-                        id: tempID
-                    }
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2018,
+                        revenue: data[i].revenue_2018
+                    };
                     break;
                 default:
                     ball = {
                         size: Math.sqrt(Math.sqrt(data[i].revenue_2010)),
                         secsize: Math.sqrt(data[i].employees_2010),
-                        id: tempID
+                        id: tempID,
+                        name: data[i].company_name,
+                        nr_employees: data[i].employees_2010,
+                        revenue: data[i].revenue_2010
                     }
             }
             graph.push(ball);
             if (tempID in anchorNodes) {
-                nodeLinks.push({ "source": anchorNodes[tempID], "target": i });
-                nodeLinks.push({ "source": i, "target": anchorNodes[tempID] });
-            }
-            else {
+                nodeLinks.push({"source": anchorNodes[tempID], "target": i});
+                nodeLinks.push({"source": i, "target": anchorNodes[tempID]});
+            } else {
                 anchorNodes[tempID] = i;
                 anchorNodesIndex[tempID] = idx;
                 idx += 5;
@@ -173,11 +209,17 @@ const MegaBallsView = ({ height = 1000, width = 1000, showBorderOfBallBox = true
         // update the simulation based on the data
         simulation
             .nodes(graph)
-            .force("collide", d3.forceCollide().strength(.2).radius(function (d) { return d.size; }).iterations(2))
+            .force("collide", d3.forceCollide().strength(.2).radius(function (d) {
+                return d.size;
+            }).iterations(2))
             .on("tick", function (d) {
                 node
-                    .attr("cx", function (d) { return d.x; })
-                    .attr("cy", function (d) { return d.y; })
+                    .attr("cx", function (d) {
+                        return d.x;
+                    })
+                    .attr("cy", function (d) {
+                        return d.y;
+                    })
             })
             .force("link", d3.forceLink(nodeLinks).strength(.1).distance(1).iterations(2).id(graph.id));
 
@@ -198,22 +240,49 @@ const MegaBallsView = ({ height = 1000, width = 1000, showBorderOfBallBox = true
             .style("fill", "None")
             .style("stroke", "#232323");
 
+        canvas
+            .append('text')
+            .attr('class', 'details')
+            .attr('x', width / 2)
+            .attr('y', height - 20)
+            .attr('text-anchor', 'middle')
+            .text(year_choice);
 
         var node = canvas.append("g")
             .attr("class", "node")
             .selectAll("circle")
             .data(graph)
             .enter().append("circle")
-            .attr("r", function (d) { return d.size; })
-            .attr("fill", function (d) { return colorScale(anchorNodesIndex[d.id] / 100) }) // m was (Object.size(anchorNodes)) before, matching Sunburst
+            .attr("r", function (d) {
+                return d.size;
+            })
+            .attr("fill", function (d) {
+                return colorScale(anchorNodesIndex[d.id] / 100)
+            }) // m was (Object.size(anchorNodes)) before, matching Sunburst
             .attr('fill-opacity', 0.8)
             .style("stroke", d => d.error ? "red" : "black")
-            .attr("cx", function (d) { return d.x + width / 2; })
-            .attr("cy", function (d) { return d.y + height / 2; })
+            .attr("cx", function (d) {
+                return d.x + width / 2;
+            })
+            .attr("cy", function (d) {
+                return d.y + height / 2;
+            })
             .call(d3.drag()
                 .on("start", dragstarted)
                 .on("drag", dragged)
-                .on("end", dragended));
+                .on("end", dragended))
+            .on('mouseover', function(d){
+                d3.selectAll('.details')
+                    .append('text')
+                    .text(function (p) {
+                        return d.name + ": " + (d.nr_employees === null ? '0' : d.nr_employees) + " employee(s) and " + d.revenue + 'SEK revenue in ' + year_choice;
+                    });
+            })
+            .on('mouseout', function (d) {
+                d3.selectAll('.details')
+                    .text(year_choice);
+            });
+
         /*
                 var node = canvas.selectAll('.node')
                     .data( graph )
@@ -271,7 +340,9 @@ const MegaBallsView = ({ height = 1000, width = 1000, showBorderOfBallBox = true
 
     }, [year_choice, ballBox.min.x, ballBox.min.y, bhm, bvm, height, margin.bottom, margin.left, margin.right, margin.top, width]); // useEffect
 
-    return <React.Fragment><svg height={height} width={width} ref={svgRef}></svg> </React.Fragment>;
+    return <React.Fragment>
+        <svg height={height} width={width} ref={svgRef}></svg>
+    </React.Fragment>;
 };
 
 export default MegaBallsView;
@@ -279,69 +350,67 @@ export default MegaBallsView;
 
 // code from bouncing balls example
 
- // function tick(e) {
-        //     force.alpha(1);
-        //     circle
-        //         .each(gravity(e.alpha))
-        //         .each(collide(0.03))
-        //         .attr("cx", function (d) { return d.x; })
-        //         .attr("cy", function (d) { return d.y; });
-        // }
+// function tick(e) {
+//     force.alpha(1);
+//     circle
+//         .each(gravity(e.alpha))
+//         .each(collide(0.03))
+//         .attr("cx", function (d) { return d.x; })
+//         .attr("cy", function (d) { return d.y; });
+// }
 
-        // // Move nodes toward cluster focus
-        // function gravity(alpha) {
-        //     return function (d) {
-        //         if ((d.x - d.radius - 2) < rect[0]) d.speedX = Math.abs(d.speedX);
-        //         if ((d.x + d.radius + 2) > rect[2]) d.speedX = -1 * Math.abs(d.speedX);
-        //         if ((d.y - d.radius - 2) < rect[1]) d.speedY = -1 * Math.abs(d.speedY);
-        //         if ((d.y + d.radius + 2) > rect[3]) d.speedY = Math.abs(d.speedY);
+// // Move nodes toward cluster focus
+// function gravity(alpha) {
+//     return function (d) {
+//         if ((d.x - d.radius - 2) < rect[0]) d.speedX = Math.abs(d.speedX);
+//         if ((d.x + d.radius + 2) > rect[2]) d.speedX = -1 * Math.abs(d.speedX);
+//         if ((d.y - d.radius - 2) < rect[1]) d.speedY = -1 * Math.abs(d.speedY);
+//         if ((d.y + d.radius + 2) > rect[3]) d.speedY = Math.abs(d.speedY);
 
-        //         d.x = d.x + (d.speedX * 0.01);
-        //         d.y = d.y + (-1 * d.speedY * 0.01);
-        //     };
-        // }
+//         d.x = d.x + (d.speedX * 0.01);
+//         d.y = d.y + (-1 * d.speedY * 0.01);
+//     };
+// }
 
-        // // Resolve collisions between nodes
-        // function collide(alpha) {
-        //     //changed from: d3.geom.quadtree 
-        //     var quadtree = d3.quadtree(nodes);
+// // Resolve collisions between nodes
+// function collide(alpha) {
+//     //changed from: d3.geom.quadtree
+//     var quadtree = d3.quadtree(nodes);
 
-        //     return function (d) {
-        //         var r = d.radius + radius.domain()[1] + padding,
-        //             nx1 = d.x - r,
-        //             nx2 = d.x + r,
-        //             ny1 = d.y - r,
-        //             ny2 = d.y + r;
+//     return function (d) {
+//         var r = d.radius + radius.domain()[1] + padding,
+//             nx1 = d.x - r,
+//             nx2 = d.x + r,
+//             ny1 = d.y - r,
+//             ny2 = d.y + r;
 
-        //         quadtree.visit(function (quad, x1, y1, x2, y2) {
-        //             if (quad.point && (quad.point !== d)) {
-        //                 var x = d.x - quad.point.x,
-        //                     y = d.y - quad.point.y,
-        //                     l = Math.sqrt(x * x + y * y),
-        //                     r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
+//         quadtree.visit(function (quad, x1, y1, x2, y2) {
+//             if (quad.point && (quad.point !== d)) {
+//                 var x = d.x - quad.point.x,
+//                     y = d.y - quad.point.y,
+//                     l = Math.sqrt(x * x + y * y),
+//                     r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
 
-        //                 if (l < r) {
-        //                     l = (l - r) / l * alpha;
-        //                     d.x -= x *= l;
-        //                     d.y -= y *= l;
-        //                     quad.point.x += x;
-        //                     quad.point.y += y;
-        //                 }
-        //             }
-        //             return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-        //         });
-        //     };
-        // };//collide
-
-
+//                 if (l < r) {
+//                     l = (l - r) / l * alpha;
+//                     d.x -= x *= l;
+//                     d.y -= y *= l;
+//                     quad.point.x += x;
+//                     quad.point.y += y;
+//                 }
+//             }
+//             return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+//         });
+//     };
+// };//collide
 
 
-        // old balls
+// old balls
 
-         // const balls = canvas.selectAll("circle")
-        //     .data(nodes)
-        //     .enter().append("circle")
-        //     .attr("r", function (d) { return d.radius })
-        //     .attr("cx", function (d) { return d.x })
-        //     .attr("cy", function (d) { return d.y; })
-        //     .style("fill", function (d) { return d.color; });
+// const balls = canvas.selectAll("circle")
+//     .data(nodes)
+//     .enter().append("circle")
+//     .attr("r", function (d) { return d.radius })
+//     .attr("cx", function (d) { return d.x })
+//     .attr("cy", function (d) { return d.y; })
+//     .style("fill", function (d) { return d.color; });
