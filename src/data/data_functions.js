9f70d314aaf19eb0b3b058acff707bb4d58c32de
hesseb <hesseb@kth.se>
2020-02-28 15:52:45 +0100

    importing new data

--- src/data/data_functions.js
+++ src/data/data_functions.js
@@ -1,3 +1,4 @@
+import companyData from '../data/companies.json';
 
 export function getRevenueForCompanyObjectByYear(companyObj, year) {
     const revenueKey = "revenue_" + year;
@@ -7,7 +8,7 @@ export function getRevenueForCompanyObjectByYear(companyObj, year) {
 
 // TO BE DONE: needs to be changed so we return category data instead of placeholder data
 export function getDataForSunburst(year) {
-    const rand1 = Math.random() * (10 - 1) + 1;
+    const rand1 = Math.random() * (10 - 1) + 8;
     const rand2 = Math.random() * (10 - 1) + 1;
     return [{ label: 'FirstObj', color: 'red', value: rand1 }, { label: 'SecondObj', color: 'blue', value: rand2 }];
 };
