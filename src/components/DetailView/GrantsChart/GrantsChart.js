267f80dd593b7d875b60e5dd9ba7cdb2523b9790
Mar Balibrea <marbalibrea8@gmail.com>
2020-02-29 17:11:19 +0100

    it works!

--- src/components/DetailView/GrantsChart/GrantsChart.js
+++ src/components/DetailView/GrantsChart/GrantsChart.js
@@ -60,6 +60,7 @@ const GrantsChart = ({ onYearClicked }) => {
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height)
+                    .append('g')
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
                     .classed('linechart_canvas', true);
 
