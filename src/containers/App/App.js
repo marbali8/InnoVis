82d914460a380fbd3d3d2a67a0445bb80270a5be
Alvin Hager <alvinhager@Alvins-Air.lan>
2020-03-01 21:36:52 +0100

    fix typo

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,7 +4,7 @@ import Infobox from '../../components/DetailView/InfoBox/Infobox.js'
 import Sunburst from '../../components/DetailView/Sunburst/RefactoredSunburst.js'
 import GrantsChart from "../../components/DetailView/GrantsChart/GrantsChart.js";
 import TimeSlider from '../../components/TimeSlider/TimeSlider.js';
-import MegaBalls from '../../components/MegaBallsView/MegaBallsView_v2.js';
+import MegaBalls from '../../components/MegaballsView/MegaBallsView_v2.js'
 import { getDataForSunburst, getDataForMegaballs } from '../../data/data_functions.js';
 
 function App() {
@@ -31,7 +31,7 @@ function App() {
                 Take a look at our alumni companies and ideas!
             </div>
             <div className={classes.megaBallsView}>
-                <MegaBalls data={getDataForMegaballs(year)} category={category} onBallMouseHover={handleCategoryBallsHover}/>
+                <MegaBalls data={getDataForMegaballs(year)} category={category} onBallMouseHover={handleCategoryBallsHover} />
             </div>
             <TimeSlider onYearClicked={handleTimeSliderYearClicked} range={[2010, 2018]} />
             <div className={classes.aggregateKTHDataView}>
