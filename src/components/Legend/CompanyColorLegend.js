b831351b1bb97dca418f1645d6807d621774d127
Jacob <jacobel@kth.se>
2020-03-06 14:20:00 +0100

    First version of legend

---
+++ src/components/Legend/CompanyColorLegend.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import classes from './CompanyColorLegend.module.scss';
+
+// getColorBycompanyCategory returns the color of the category, taking in the category id as input, where there are 10 categories in total.
+import { getColorByCompanyCategory } from './../../utility_functions/ColorFunctions.js';
+
+// this contains an array of categories, where each elem has an id as an identifier and a label in swedish
+import categories from '../../data/category_label_mapping.json';
+
+const CompanyLabels = (dat = [], { height, width }) => {
+    
+    // function that takes in a category element and outputs a row in the company color legend
+    const getLegendForCategory = (category) => {
+
+        // this circle needs to change color based on category using getColorByCompanyCategory(category.id);
+        const color = getColorByCompanyCategory(category.id);
+        const circle = <div style={{ display: 'inline-block', width: '20px', height: '20px', backgroundColor: color, borderRadius: '50%' }} />;
+
+        return <div className={classes.categoryRow}>
+            {circle}
+            <div className={classes.label}>
+                <div><pre>{JSON.stringify(category.label,null, 2)}</pre></div>
+            </div>
+        </div>
+    };
+
+    return (<div className={classes.container}>
+        {
+            categories.map(category => getLegendForCategory(category))
+        }
+    </div>);
+};
+
+export default CompanyLabels;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,7 +5,8 @@ import Sunburst from '../../components/DetailView/Sunburst/RefactoredSunburst.js
 import GrantsChart from "../../components/DetailView/GrantsChart/GrantsChart.js";
 import TimeSlider from '../../components/TimeSlider/TimeSlider.js';
 import MegaBalls from '../../components/MegaBallsView2/MegaBallsView_v2.js'
-import CompanyLabel from '../../components/Legend/CompanyLabels.js'
+//import CompanyLabel from '../../components/Legend/CompanyLabels.js'
+import CompanyLabel from '../../components/Legend/CompanyColorLegend.js'
 import BallsLegend from '../../components/MegaBallsView2/legend.js'
 import { getDataForSunburst, getDataForMegaballs } from '../../data/data_functions.js';
 
@@ -53,8 +54,8 @@ function App() {
                 </div>
             </div>
 
-            <div className = {classes.CompanyLabels}>
-                <CompanyLabel data={megaballData}></CompanyLabel>
+            <div className={classes.CompanyLabel}> 
+                <CompanyLabel data = {megaballData}></CompanyLabel>
             </div>
 
             <div className={classes.footer}>
@@ -64,6 +65,10 @@ function App() {
         </div>
     );
 }
+
+/*            <div className = {classes.CompanyLabel}>
+                <CompanyLabel data={megaballData}></CompanyLabel>
+            </div>*/
 
 export default App;
 
