a71b96879802a6abe451a199dee1425cd0867a45
Alvin Hager <alvinhager@n134-p136.eduroam.kth.se>
2020-03-09 18:44:00 +0100

    added infobox

--- src/components/InfoButton/InfoButton.js
+++ src/components/InfoButton/InfoButton.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './InfoButton.module.scss';
 
 const InfoButton = () => {
-    return <div className={classes.InfoButton}>i</div>;
+
+    const [showButton, setShowButton] = useState(false);
+
+    return <div>
+        <div onClick={() => { setShowButton(!showButton) }} className={classes.InfoButton}>
+            <div className={classes.button}>?</div>
+            {showButton && <div className={classes.popupBox}>
+                <div className={classes.bubble}>
+                    <div className={classes.introText}>
+                        <p>The circles represent startup companies KTH innovation has helped create and foster</p>
+                        <p>The circle's area is proportional to the total revenue of that company during the active year, which is selected with the time slider </p>
+                        <p>Hover on the circles to find out more about each company!</p>
+                    </div>
+                    <div className={classes.footer}>
+
+                        <p>This data was taken from tax revenue info. provided by KTH Innovation. The visualisation was developed as part of the Information Visualization course at KTH. Group members are: <a
+                            href="mailto:hesseb@kth.se">Alexander Hesseborn</a>, <a href="mailto:alvinh@kth.se">Alvin
+                        Häger</a>, <a href="mailto:sonebo@kth.se">Christina Sonebo</a>, <a href="mailto:hcbayat@kth.se">Hannah
+                        Clara Bayat</a>, <a href="mailto:jacobel@kth.se">Jacob von Eckermann</a> and <a
+                                href="mailto:marbr@kth.se">Mar Balibrea</a>. </p>
+                    </div>
+
+
+                </div>
+            </div>}
+        </div>
+
+    </div>
 };
 
 export default InfoButton;
